f2640bf6f03f165f130f9c2117f458594e1fca58
AlexL <spookspook@mail.ru>
2025-12-28 14:40:36 +0300

    auto create superuser for testing

--- myshop/shop/apps.py
+++ myshop/shop/apps.py
@@ -1,5 +1,24 @@
 from django.apps import AppConfig
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError, ProgrammingError
 
 
 class ShopConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'shop'
+
+    def ready(self):
+        try:
+            User = get_user_model()
+
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@test.local',
+                    password='admin12345'
+                )
+                print('✅ Test superuser created: admin / admin12345')
+
+        except (OperationalError, ProgrammingError):
+            # база ещё не готова — пропускаем
+            pass

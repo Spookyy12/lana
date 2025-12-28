97fb1778447566345010f51ce3125ffb9f76cc0e
AlexL <spookspook@mail.ru>
2025-12-28 14:56:55 +0300

    s7dfyuis

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,10 +1,18 @@
-from django.contrib.auth import views as auth_views
-from shop import views as shop_views 
+from django.contrib import admin
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    
-    path('login/', auth_views.LoginView.as_view(template_name='shop/registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-    path('register/', shop_views.register, name='register'),
-    
+    path('admin/', admin.site.urls),
+    path('', include('shop.urls')),
+    path('', include('users.urls')),  # если у тебя auth отдельно
 ]
+
+# 🔥 ВОТ ЭТО ЧИНИТ ФОТО
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
